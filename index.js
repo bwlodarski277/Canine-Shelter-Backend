62d02a13e96ac2795254c3d60b5783612834f7b0
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-02-06 17:59:05 +0000

    Added basic routes to index

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = new Koa();
 const special = require('./routes/special');
 const users = require('./routes/users');
 const dogs = require('./routes/dogs');
+const breeds = require('./routes/breeds');
+const locations = require('./routes/locations');
 
 app.use(special.routes());
 app.use(users.routes());
 app.use(dogs.routes());
+app.use(breeds.routes());
+app.use(locations.routes());
 
 const port = process.env.PORT || 3000;
 
