2af88c589b3cd69ffb16fd0bacb6cc8293365177
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-02-16 16:19:24 +0000

    Updated users route to use DB.

--- routes/users.js
+++ routes/users.js
@@ -1,110 +1,68 @@
 const Router = require('koa-router');
 const bodyParser = require('koa-bodyparser');
 
+const userModel = require('../models/users');
+const favsModel = require('../models/favourites');
+
 const router = new Router({ prefix: '/api/v1/users' });
-
-let users = [
-    {
-        username: 'aTuring',
-        email: 'aTuring@example.com',
-        firstName: 'Alan',
-        lastName: 'Turing',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/a/a1/Alan_Turing_Aged_16.jpg',
-        dateCreated: '10/02/2013',
-        dateModified: '10/02/2013'
-    },
-    {
-        username: 'tBLee',
-        email: 'tBLee@example.com',
-        firstName: 'Tim',
-        lastName: 'Berners-Lee',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Sir_Tim_Berners-Lee_%28cropped%29.jpg',
-        dateCreated: '15/05/2017',
-        dateModified: '22/07/2018'
-    },
-    {
-        username: 'JvNeumann',
-        email: 'JvNeumann@example.com',
-        firstName: 'John',
-        lastName: 'Neumann',
-        imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/5/5e/JohnvonNeumann-LosAlamos.gif',
-        dateCreated: '10/02/2013',
-        dateModified: '10/02/2013'
-    }
-]
-
-/**
- * Creates a date string.
- */
-async function getCurrentDate() {
-    const now = new Date();
-    const date = `${now.getDate()}/${now.getMonth()}/${now.getFullYear()}`;
-    return date;
-}
-
-/**
- * Checks if an ID is valid.
- * @param {int} id ID to check
- */
-const isValid = id => id < users.length + 1 && id > 0;
 
 router.get('/', getAll);
 router.post('/', bodyParser(), createUser);
 
 router.get('/:id([0-9]{1,})', getUser);
-router.put('/:id([0-9]{1,})', updateUser);
+router.put('/:id([0-9]{1,})', bodyParser(), updateUser);
 router.del('/:id([0-9]{1,})', deleteUser);
+
+router.get('/:id([0-9]{1,})/favourites', getUserFavs);
+router.post('/:id([0-9]{1,})/favourites', bodyParser(), addUserFav);
+
+router.get('/:id([0-9]{1,})/favourites/:dogId', getUserFav);
+router.del('/:id([0-9]{1,})/favourites/:dogId', deleteUserFav);
 
 /**
  * Gets all the users from the database.
  */
 async function getAll(ctx) {
-    ctx.body = users;
+    ctx.body = await userModel.getAll();
 }
 
 /**
  * Gets a single user from the database.
  */
 async function getUser(ctx) {
-    const id = parseInt(ctx.params.id);
-    if (isValid(id))
-        ctx.body = users[id - 1];
-    else
-        ctx.status = 404;
+    const id = ctx.params.id;
+    const user = await userModel.getById(id);
+    if (user) {
+        ctx.body = user;
+    }
 }
 
 /**
  * Adds a user to the database.
  */
 async function createUser(ctx) {
-    const { username, email, firstName, lastName, imageUrl } = ctx.request.body;
-    const date = await getCurrentDate();
-    // Creating a new user
-    let newUser = {
-        username, email, firstName, lastName, dateCreated: date,
-        dateModified: date, imageUrl
-    };
-    users.push(newUser);
-    ctx.status = 201;
-    ctx.body = newUser;
+    const body = ctx.request.body;
+    const id = await userModel.add(body);
+    if (id) {
+        ctx.status = 201;
+        ctx.body = { ID: id, created: true, link: `${ctx.request.path}/${id}` };
+    }
 }
 
 /**
  * Updates a user in the database.
  */
 async function updateUser(ctx) {
-    const id = parseInt(ctx.params.id);
-    if (isValid(id)) {
-        const user = users[id - 1];
-        // Constructing updated user
-        const { username, email, firstName, lastName, imageUrl } = ctx.request.body;
-        const { dateCreated } = user;
-        const dateModified = await getCurrentDate();
-        const updatedUser = { username, email, firstName, lastName, imageUrl, dateCreated, dateModified };
-
-        ctx.body = updatedUser;
-    } else {
-        ctx.status = 404;
+    const user_id = ctx.params.id;
+    let user = await userModel.getById(user_id);
+    if (user) {
+        // Excluding fields that must not be updated
+        const { id, dateCreated, ...body } = ctx.request.body;
+        Object.assign(user, body); // overwriting everything else
+        const result = await userModel.update(user_id, user);
+        if (result) { // Knex returns amount of affected rows.
+            ctx.body = { id: user_id, updated: true, link: ctx.request.path };
+        }
     }
 }
 
@@ -112,12 +70,48 @@ async function updateUser(ctx) {
  * Deletes a user from the database.
  */
 async function deleteUser(ctx) {
-    const id = parseInt(ctx.params.id);
-    if (isValid(id)) {
-        users.splice(id - 1, 1);
+    const id = ctx.params.id;
+    let user = await userModel.getById(id);
+    if (user) {
+        const result = await userModel.delete(id);
+        if (result) {
+            ctx.status = 200;
+        }
+    }
+}
+
+async function getUserFavs(ctx) {
+    const id = ctx.params.id;
+    let favs = await favsModel.getByUserId(id);
+    if (favs) {
+        ctx.body = favs
+    }
+}
+
+async function addUserFav(ctx) {
+    const userId = ctx.params.id;
+    const { dogId } = ctx.request.body;
+    const result = await favsModel.add(userId, dogId);
+    if (result === 0) { // 0 since add returns PK of inserted row
+        ctx.status = 201;
+    }
+}
+
+async function getUserFav(ctx) {
+    const userId = ctx.params.id;
+    const dogId = ctx.params.dogId;
+    const result = await favsModel.getByDogId(userId, dogId);
+    if (result) {
+        ctx.body = result;
+    }
+}
+
+async function deleteUserFav(ctx) {
+    const userId = ctx.params.id;
+    const dogId = ctx.params.dogId;
+    const result = await favsModel.delete(userId, dogId);
+    if (result) {
         ctx.status = 200;
-    } else {
-        ctx.status = 404;
     }
 }
 
