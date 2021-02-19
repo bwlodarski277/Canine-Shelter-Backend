ee3ea37ee278c4b0302e3ccb4d60b1dec9e77a9f
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-02-19 21:58:54 +0000

    Added login route for JWT

--- index.js
+++ index.js
@@ -7,12 +7,15 @@ const users = require('./routes/users');
 const dogs = require('./routes/dogs');
 const breeds = require('./routes/breeds');
 const locations = require('./routes/locations');
+const login = require('./routes/login');
 
 app.use(special.routes());
 app.use(users.routes());
 app.use(dogs.routes());
 app.use(breeds.routes());
 app.use(locations.routes());
+
+app.use(login.routes());
 
 const port = process.env.PORT || 3000;
 
