cec7c6dcef75f06299269e61bbf36555588045b2
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-02-21 11:17:04 +0000

    Gave messages PKs

--- sql/messages.sql
+++ sql/messages.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `messages` (
+    `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
     `userId` int,
     `staffId` int,
-    `dateCreated` datetime,
+    `dateCreated` datetime DEFAULT CURRENT_TIMESTAMP,
     `message` text NOT NULL,
     FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
-    FOREIGN KEY (`staffId`) REFERENCES `staff` (`id`),
-    PRIMARY KEY (`userId`, `staffId`)
+    FOREIGN KEY (`staffId`) REFERENCES `staff` (`id`)
 );
