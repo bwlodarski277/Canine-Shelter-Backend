21f4142702c433d5d059bb8ca8022db4bd8d99af
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-02-21 17:07:22 +0000

    Created and updated message routes

---
+++ sql/chats.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `chats` (
+    `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `locationId` int NOT NULL,
+    `userId` int NOT NULL,
+    FOREIGN KEY (`locationId`) REFERENCES `locations` (`id`),
+    FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
+);
