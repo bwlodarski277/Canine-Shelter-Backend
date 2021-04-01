23c82c512c1589e8e9443aefc9a84bdb34de37b4
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-01 18:04:18 +0000

    Updated docs

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -1,12 +1,12 @@
 function scrollToNavItem() {
-	let path = window.location.href
-		.split('/')
-		.pop()
-		.replace(/\.html/, '');
-	document.querySelectorAll('nav a').forEach(link => {
-		let href = link.attributes.href.value.replace(/\.html/, '');
-		if (path === href) link.scrollIntoView({ block: 'center' });
-	});
-}
+    var path = window.location.href.split('/').pop().replace(/\.html/, '');
+    document.querySelectorAll('nav a').forEach(function(link) {
+      var href = link.attributes.href.value.replace(/\.html/, '');
+      if (path === href) {
+        link.scrollIntoView({block: 'center'});
+        return;
+      }
+    })
+  }
 
-scrollToNavItem();
+  scrollToNavItem();
