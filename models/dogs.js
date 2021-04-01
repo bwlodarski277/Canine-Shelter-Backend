09e51227027c806752b70766ab5afc6d30682172
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-01 18:05:20 +0000

    Removed filters

--- models/dogs.js
+++ models/dogs.js
@@ -24,37 +24,24 @@ const { db, run } = require('../helpers/database');
 /**
  * Get a list of dog entries from the DB.
  * Allows for searching, filtering and sorting.
- * @param {string} [query] search for dogs by name and description.
- * @param {number} [page=1] which page to get data from.
- * @param {number} [limit=50] number of items on a page.
- * @param {string} [order='id'] what parameter to order by.
- * @param {'asc'|'desc'} [direction='asc'] direction to sort (asc. or desc.).
- * @param {object} [filters] object of filters from the query.
+ * @param {string} query search for dogs by name and description.
+ * @param {Array<string>} select list of columns to select.
+ * @param {number} page which page to get data from.
+ * @param {number} limit number of items on a page.
+ * @param {string} order what parameter to order by.
+ * @param {'asc'|'desc'} direction direction to sort (asc. or desc.).
  * @returns {Promise<Array<Dog>>} array of selected dog records.
  * @async
  */
-exports.getAll = async (
-	query = '',
-	page = 1,
-	limit = 50,
-	order = 'id',
-	direction = 'asc',
-	filters = {}
-) => {
-	direction = direction === 'desc' ? 'desc' : 'asc'; // If direction is anything but descending, use ascending.
+exports.getAll = async (query, select, page, limit, order, direction) => {
 	const offset = (page - 1) * limit;
 	const data = await run(
 		async () =>
 			await db('dogs')
-				.where(filters) // applying user filters (like age, gender).
-				.andWhere((
-					builder // ... WHERE <filters> AND (<name> OR <description>)
-				) =>
-					// Applying search query
-					builder
-						.where('name', 'like', `%${query}%`) // Safe as it is escaped by driver
-						.orWhere('description', 'like', `%${query}%`)
-				) // same as line above
+				.select(...select)
+				// Safe as it is escaped by driver
+				.where('name', 'like', `%${query}%`)
+				.orWhere('description', 'like', `%${query}%`)
 				.orderBy(order, direction)
 				.limit(limit)
 				.offset(offset)
