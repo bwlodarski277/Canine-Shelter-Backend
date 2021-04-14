004b9a3bc5af886cf492d046db3b23a6753f5760
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-14 15:48:15 +0100

    Added file to string utility function

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const fs = require('fs');
+
 /**
  * Restricts a value between a minimum and maximum.
  * @param {number} value value to clamp.
@@ -8,3 +10,10 @@
  * @returns {number} clamped value.
  */
 exports.clamp = (value, min, max) => Math.max(min, Math.min(value, max));
+
+/**
+ * Reads a file as a string.
+ * @param {string} path path at which the file is located
+ * @returns String from the file read
+ */
+exports.stringFromFile = path => fs.readFileSync(fs.realpathSync(path)).toString();
