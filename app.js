68aacd727e6b3932de422d309b64f17cf583856d
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-04-15 13:19:08 +0100

    Split index into app and index

---
+++ app.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const Koa = require('koa');
+const cors = require('@koa/cors');
+
+const app = new Koa();
+
+const special = require('./routes/special');
+const users = require('./routes/users');
+const dogs = require('./routes/dogs');
+const breeds = require('./routes/breeds');
+const locations = require('./routes/locations');
+const auth = require('./routes/auth');
+
+// Allowing the frontend box to access the API.
+// const options = { origin: 'https://latin-kimono-3000.codio-box.uk' };
+
+app.use(cors());
+
+app.use(special.routes());
+app.use(users.routes());
+app.use(dogs.routes());
+app.use(breeds.routes());
+app.use(locations.routes());
+app.use(auth.routes());
+
+module.exports = app;
