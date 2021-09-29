50fc9313aa8904d43cea47d0e137d05267c7959d
bwlodarski277 <b.wlodarski277@gmail.com>
2021-09-29 17:05:03 +0100

    Added more appropriate dog names

--- db/samples/002_seed_dogs.js
+++ db/samples/002_seed_dogs.js
@@ -4,15 +4,15 @@
 const Knex = require('knex');
 
 const dogs = [
-	{ name: 'Rex', description: 'A good boi', age: 5, gender: 1, imageUrl: 'sampleUrl' },
-	{ name: 'Woofer', description: 'A woofy boi', age: 3, gender: 1, imageUrl: 'sampleUrl' },
-	{ name: 'Luna', description: 'A good grill', age: 2, gender: 0, imageUrl: 'sampleUrl' },
-	{ name: 'Doge', description: 'A good boi', age: 10, gender: 1, imageUrl: 'sampleUrl' },
-	{ name: 'Rex2', description: 'A good boi', age: 5, gender: 1, imageUrl: 'sampleUrl' },
-	{ name: 'Woofer2', description: 'A woofy boi', age: 3, gender: 1, imageUrl: 'sampleUrl' },
-	{ name: 'Luna2', description: 'A good grill', age: 2, gender: 0, imageUrl: 'sampleUrl' },
-	{ name: 'Doge2', description: 'A good boi', age: 10, gender: 1, imageUrl: 'sampleUrl' },
-	{ name: 'Doge3', description: 'A good boi', age: 10, gender: 1, imageUrl: 'sampleUrl' }
+	{ name: 'Rex', description: 'A good boi', age: 5, gender: 1 },
+	{ name: 'Woofer', description: 'A woofy boi', age: 3, gender: 1 },
+	{ name: 'Luna', description: 'A good grill', age: 2, gender: 0 },
+	{ name: 'Doge', description: 'A good boi', age: 10, gender: 1 },
+	{ name: 'Maximilian', description: 'A good boi', age: 5, gender: 1 },
+	{ name: 'Spot', description: 'A woofy boi', age: 3, gender: 1 },
+	{ name: 'Bean', description: 'A good grill', age: 2, gender: 0 },
+	{ name: 'Copper', description: 'A good boi', age: 10, gender: 1 },
+	{ name: 'Gerald', description: 'A good boi', age: 10, gender: 1 }
 ];
 
 /**
